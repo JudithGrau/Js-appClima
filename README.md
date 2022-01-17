1953d85c90909a3b13f739c6b9ce0bef46eb6bef
JudithGrau <judith.betina.grau@gmail.com>
2022-01-16 21:35:21 -0300

    Se agrega el proyecto

---
+++ README.md
@@ -0,0 +1,4 @@
+# Js-appClima
+<p align="center">
+    <img width="600" src="/assets/to_readme/App-Clima.gif">
+</p>
---
+++ index.js
@@ -0,0 +1,40 @@
+const API_KEY = 'f880c7b7cf2334958a32a8a37652ab1b';
+const fetchData = position => {
+    const { latitude, longitude } = position.coords;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => setWeatherData(data))
+}
+
+const setWeatherData = data => {
+    console.log(data);
+    const weatherData = {
+        location: data.name,
+        description: data.weather[0].main,
+        humidity: data.main.humidity,
+        pressure: data.main.pressure,
+        temperature: data.main.temp,
+        date: getDate(),
+    }
+
+    Object.keys(weatherData).forEach( key => {
+        document.getElementById(key).textContent = weatherData[key];
+    });
+    cleanUp();
+}
+const cleanUp = () => {
+    let container = document.getElementById('container');
+    let loader = document.getElementById('loader');
+
+    loader.style.display = 'none';
+    container.style.display = 'flex';
+}
+
+const getDate = () => {
+    let date = new Date();
+    return `${date.getDate()}-${ ( '0' + ( + 1)).slice(-2) }-${date.getFullYear()}`;
+}
+
+const onLoad = () => {
+    navigator.geolocation.getCurrentPosition(fetchData);
+}
